57c5d3064186fbd62733a7342eeb406235150a1a
YunA486 <w2039@e-mirim.hs.kr>
2021-09-22 23:26:51 +0900

    Edit Header.html

--- separateCollection/separateJsp.js
+++ separateCollection/separateJsp.js
@@ -18,15 +18,3 @@ function myFunction() {
   window.location.href = "http://cybertramp.net/search/"+x;
 }
 
-function btnclick(_url){
-    $.ajax({
-        url : _url,
-        type : 'post',
-        success: function(data) {
-            $('#include_page').html(data);
-        },
-            error: function() {
-            $('#include_page').text('페이지 점검중 입니다.');
-        }
-    });
-}
