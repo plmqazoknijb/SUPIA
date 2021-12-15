55b74eb9f567d8a6acf65f2a9ffc8cb48651a14f
plmqazoknijb <w2033@e-mirim.hs.kr>
2021-12-15 23:09:40 +0900

    업사이클 2 자동 슬라이더

---
+++ upcycle/updycle.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $('.more').click(function(){
+//       if($('.more').hasClass('more')){
+//          $('.more').addClass('close').removeClass('more');
+//          $('.b').css('visibility', 'visible');
+//       }else if($('.close').hasClass('close')){
+//          $('.close').addClass('more').removeClass('close');  
+//          $('.b').css('visibility', 'hidden');
+//       }
+//     });
+//   });
